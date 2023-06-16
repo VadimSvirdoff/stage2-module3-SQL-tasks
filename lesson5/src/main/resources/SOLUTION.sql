8a6ed72660c349e601dc3269e177ea9b11d8cc38
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-16 18:21:50 +0300

    fix

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,8 +1,8 @@
 SELECT * FROM payment WHERE amount >= 500;
-SELECT * FROM student WHERE birthday < DATEADD(YEAR, -20, GETDATE());
-SELECT * FROM student WHERE groupnumber = 10 AND birthday > DATEADD(YEAR, -20, GETDATE());
+SELECT * FROM student WHERE birthday < DATE_ADD(CURRENT_DATE, INTERVAL -20 YEAR);
+SELECT * FROM student WHERE groupnumber = 10 AND birthday > DATE_ADD(CURRENT_DATE, INTERVAL -20 YEAR);
 SELECT * from student WHERE name = 'Mike' OR groupnumber IN (4,5,6);
-SELECT * FROM payment WHERE payment_date >= DATEADD(MONTH, -8, GETDATE());
+SELECT * FROM payment WHERE payment_date >= DATE_ADD(CURRENT_DATE, INTERVAL -8 YEAR);
 SELECT * FROM student WHERE name LIKE 'A%';
 SELECT * FROM student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
 
