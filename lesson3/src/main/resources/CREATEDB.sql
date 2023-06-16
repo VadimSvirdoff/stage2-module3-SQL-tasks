3cc314815f69642cfd61d845779a1149d26c2915
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-16 18:11:16 +0300

    fix

--- lesson3/src/main/resources/CREATEDB.sql
+++ lesson3/src/main/resources/CREATEDB.sql
@@ -5,22 +5,23 @@ CREATE TABLE student
     birthday DATE NOT NULL,
     groupnumber INT NOT NULL
 );
+
 CREATE TABLE subject
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(250),
     description VARCHAR(255),
-    grade int NOT NULL CHECK (mark >= 1 AND mark <= 5)
+    grade INT NOT NULL CHECK (grade >= 1 AND grade <= 5)
 );
 
 CREATE TABLE mark
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    student_id BIGINT,
-    subject_id BIGINT,
+    student_id BIGINT NOT NULL,
+    subject_id BIGINT NOT NULL,
     mark INT CHECK (mark >= 1 AND mark <= 10),
-    foreign key (student_id) references student(id),
-    foreign key (subject_id) references subject(id)
+    FOREIGN KEY (student_id) REFERENCES student(id),
+    FOREIGN KEY (subject_id) REFERENCES subject(id)
 );
 
 CREATE TABLE paymenttype
@@ -33,10 +34,9 @@ CREATE TABLE payment
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     type_id BIGINT NOT NULL,
-    amount decimal NOT NULL,
-    payment_date DateTime NOT NULL,
+    amount DECIMAL NOT NULL,
+    payment_date DATETIME NOT NULL,
     student_id BIGINT NOT NULL,
-    foreign key (type_id) references paymenttype(id),
-    foreign key (student_id) references student(id)
+    FOREIGN KEY (type_id) REFERENCES paymenttype(id),
+    FOREIGN KEY (student_id) REFERENCES student(id)
 );
-
