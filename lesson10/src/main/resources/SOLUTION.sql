1959ebfc858f0f5c60d019b00cc189337fe5d966
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-16 18:01:12 +0300

    fix

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,3 +1,3 @@
-SELECT * FROM Subject WHERE (SELECT AVG(mark) FROM Mark WHERE Mark.subject_id = Subject.id) > (SELECT AVG(mark) FROM Mark);
+SELECT * FROM subject WHERE (SELECT AVG(mark) FROM mark WHERE mark.subject_id = subject.id) > (SELECT AVG(mark) FROM mark);
 
-SELECT * FROM Students WHERE id IN (SELECT student_id FROM Payments GROUP BY student_id HAVING SUM(amount) < (SELECT AVG(amount) FROM Payments));
+SELECT * FROM student WHERE id IN (SELECT student_id FROM payment GROUP BY student_id HAVING SUM(amount) < (SELECT AVG(amount) FROM payment));
